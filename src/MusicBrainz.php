45b7546bbb52d68b105aeb18d4075d4349701c22
aportela <766f6964+github@gmail.com>
2022-04-19 22:48:47 +0200

    added main class

---
+++ src/MusicBrainz.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace aportela\MusicBrainzWrapper;
+
+class MusicBrainz
+{
+    const USER_AGENT = "MusicBrainzWrapper - https://github.com/aportela/musicbrainz-wrapper (766f6964+github@gmail.com)";
+}
