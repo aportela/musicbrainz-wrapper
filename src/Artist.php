2decfea2d3276a0c82f516b01873a81309ff0bf4
aportela <766f6964+github@gmail.com>
2022-04-19 23:36:18 +0200

    added api format (json/xml) configuration

--- src/Artist.php
+++ src/Artist.php
@@ -11,77 +11,79 @@ class Artist extends \aportela\MusicBrainzWrapper\Entity
     const XML_API_URL = "https://musicbrainz.org/ws/2/artist/%s?inc=aliases";
     const JSON_API_URL = "https://musicbrainz.org/ws/2/artist/%s?inc=aliases&fmt=json";
 
-    public function SEARCHXML(string $name, int $limit = 1): array
+    public function search(string $name, int $limit = 1): array
     {
-        $response = $this->http->GET(sprintf(self::XML_SEARCH_API_URL, urlencode($name), $limit));
-        if ($response->code == 200) {
-            $xml = simplexml_load_string($response->body);
-            if ($xml->{"artist-list"} && $xml->{"artist-list"}['count'] > 0 && $xml->{"artist-list"}->{"artist"}) {
-                $results = [];
-                foreach ($xml->{"artist-list"}->{"artist"} as $artist) {
-                    $results[] = (object) [
-                        "mbId" => (string) isset($artist["id"]) ? $artist["id"] : null,
-                        "name" => (string) isset($artist->{"name"}) ? $artist->{"name"} : null,
-                        "country" => (string) isset($artist->{"country"}) ? $artist->{"country"} : null
-                    ];
+        if ($this->apiFormat == \aportela\MusicBrainzWrapper\Entity::API_FORMAT_XML) {
+            $response = $this->http->GET(sprintf(self::XML_SEARCH_API_URL, urlencode($name), $limit));
+            if ($response->code == 200) {
+                $xml = simplexml_load_string($response->body);
+                if ($xml->{"artist-list"} && $xml->{"artist-list"}['count'] > 0 && $xml->{"artist-list"}->{"artist"}) {
+                    $results = [];
+                    foreach ($xml->{"artist-list"}->{"artist"} as $artist) {
+                        $results[] = (object) [
+                            "mbId" => isset($artist["id"]) ? (string) $artist["id"] : null,
+                            "name" => isset($artist->{"name"}) ? (string) $artist->{"name"} : null,
+                            "country" => isset($artist->{"country"}) ? (string) $artist->{"country"} : null
+                        ];
+                    }
+                    return ($results);
+                } else {
+                    return ([]);
                 }
-                return ($results);
+            } else {
+                return ([]);
+            }
+        } else if ($this->apiFormat == \aportela\MusicBrainzWrapper\Entity::API_FORMAT_JSON) {
+            $response = $this->http->GET(sprintf(self::JSON_SEARCH_API_URL, urlencode($name), $limit));
+            if ($response->code == 200) {
+                $json = json_decode($response->body);
+                if ($json->{"count"} > 0 && is_array($json->{"artists"}) && count($json->{"artists"}) > 0) {
+                    $results = [];
+                    foreach ($json->{"artists"} as $artist) {
+                        $results[] = (object) [
+                            "mbId" => isset($artist->{"id"}) ? (string) $artist->{"id"} : null,
+                            "name" => isset($artist->{"name"}) ? (string) $artist->{"name"} : null,
+                            "country" => isset($artist->{"country"}) ? (string) $artist->{"country"} : null
+                        ];
+                    }
+                    return ($results);
+                } else {
+                    return ([]);
+                }
             } else {
                 return ([]);
             }
         } else {
-            return ([]);
+            throw new \aportela\MusicBrainzWrapper\Exception\InvalidAPIFormat("");
         }
     }
 
-    public function SEARCHJSON(string $name, int $limit = 1): array
+    public function get(string $mbId): string
     {
-        $response = $this->http->GET(sprintf(self::JSON_SEARCH_API_URL, urlencode($name), $limit));
-        if ($response->code == 200) {
-            $json = json_decode($response->body);
-            if ($json->{"count"} > 0 && is_array($json->{"artists"}) && count($json->{"artists"}) > 0) {
-                $results = [];
-                foreach ($json->{"artists"} as $artist) {
-                    $results[] = (object) [
-                        "mbId" => (string) isset($artist->{"id"}) ? $artist->{"id"} : null,
-                        "name" => (string) isset($artist->{"name"}) ? $artist->{"name"} : null,
-                        "country" => (string) isset($artist->{"country"}) ? $artist->{"country"} : null
-                    ];
-                }
-                return ($results);
+        if ($this->apiFormat == \aportela\MusicBrainzWrapper\Entity::API_FORMAT_XML) {
+            $response = $this->http->GET(sprintf(self::XML_API_URL, $mbId));
+            if ($response->code == 200) {
+                return ($response->body);
+            } else if ($response->code == 400) {
+                throw new \aportela\MusicBrainzWrapper\Exception\InvalidIdentifierException($mbId, $response->code);
+            } else if ($response->code == 404) {
+                throw new \aportela\MusicBrainzWrapper\Exception\NotFoundException($mbId, $response->code);
             } else {
-                return ([]);
+                throw new \Exception($mbId, $response->code);
+            }
+        } else if ($this->apiFormat == \aportela\MusicBrainzWrapper\Entity::API_FORMAT_JSON) {
+            $response = $this->http->GET(sprintf(self::JSON_API_URL, $mbId));
+            if ($response->code == 200) {
+                return ($response->body);
+            } else if ($response->code == 400) {
+                throw new \aportela\MusicBrainzWrapper\Exception\InvalidIdentifierException($mbId, $response->code);
+            } else if ($response->code == 404) {
+                throw new \aportela\MusicBrainzWrapper\Exception\NotFoundException($mbId, $response->code);
+            } else {
+                throw new \Exception($mbId, $response->code);
             }
         } else {
-            return ([]);
-        }
-    }
-
-    public function GETXML(string $mbId): string
-    {
-        $response = $this->http->GET(sprintf(self::XML_API_URL, $mbId));
-        if ($response->code == 200) {
-            return ($response->body);
-        } else if ($response->code == 400) {
-            throw new \aportela\MusicBrainzWrapper\Exception\InvalidIdentifierException($mbId, $response->code);
-        } else if ($response->code == 404) {
-            throw new \aportela\MusicBrainzWrapper\Exception\NotFoundException($mbId, $response->code);
-        } else {
-            throw new \Exception($mbId, $response->code);
-        }
-    }
-
-    public function GETJSON(string $mbId): string
-    {
-        $response = $this->http->GET(sprintf(self::JSON_API_URL, $mbId));
-        if ($response->code == 200) {
-            return ($response->body);
-        } else if ($response->code == 400) {
-            throw new \aportela\MusicBrainzWrapper\Exception\InvalidIdentifierException($mbId, $response->code);
-        } else if ($response->code == 404) {
-            throw new \aportela\MusicBrainzWrapper\Exception\NotFoundException($mbId, $response->code);
-        } else {
-            throw new \Exception($mbId, $response->code);
+            throw new \aportela\MusicBrainzWrapper\Exception\InvalidAPIFormat("");
         }
     }
 }
--- src/Entity.php
+++ src/Entity.php
@@ -7,14 +7,23 @@ use stdClass;
 class Entity
 {
     const USER_AGENT = "MusicBrainzWrapper - https://github.com/aportela/musicbrainz-wrapper (766f6964+github@gmail.com)";
+    const API_FORMAT_XML = "xml";
+    const API_FORMAT_JSON = "json";
 
     protected $logger;
     protected $http;
+    protected $apiFormat;
 
-    public function __construct(\Psr\Log\LoggerInterface $logger)
+    public function __construct(\Psr\Log\LoggerInterface $logger, string $apiFormat)
     {
         $this->logger = $logger;
         $this->logger->debug("MusicBrainzWrapper::__construct");
+        $supportedApiFormats = [ self::API_FORMAT_XML, self::API_FORMAT_JSON ];
+        if (! in_array($apiFormat, $supportedApiFormats)) {
+            $this->logger->critical("MusicBrainzWrapper::__construct ERROR: invalid api format");
+            throw new \aportela\MusicBrainzWrapper\Exception\InvalidAPIFormat("supported formats: " . implode(", ", $supportedApiFormats));
+        }
+        $this->apiFormat = $apiFormat;
         $loadedExtensions = get_loaded_extensions();
         if (!in_array("libxml", $loadedExtensions)) {
             $this->logger->critical("MusicBrainzWrapper::__construct ERROR: libxml extension not found");
