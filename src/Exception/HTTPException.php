ba7254e3514a564dfe8e6a321385fc1b8bc99b3b
aportela <766f6964+github@gmail.com>
2022-04-21 00:22:30 +0200

    added new exceptions

---
+++ src/Exception/HTTPException.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace aportela\MusicBrainzWrapper\Exception;
+
+    class HTTPException extends \Exception
+    {
+        public function __construct(string $message = "", int $code = 0, \Exception $previous = null)
+        {
+            parent::__construct($message, $code, $previous);
+        }
+
+        public function __toString()
+        {
+            return __CLASS__ . ": [{$this->code}]: {$this->message}" . PHP_EOL;
+        }
+    }
