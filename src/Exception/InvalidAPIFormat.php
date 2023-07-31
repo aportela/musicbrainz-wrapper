8cc5a194cc9e5a940f36dc5ef8e515a11477a9aa
alex <766f6964+github@gmail.com>
2023-07-31 11:47:31 +0200

    added return types

--- src/Exception/InvalidAPIFormat.php
+++ src/Exception/InvalidAPIFormat.php
@@ -1,16 +1,16 @@
 <?php
 
-    namespace aportela\MusicBrainzWrapper\Exception;
+namespace aportela\MusicBrainzWrapper\Exception;
 
-    class InvalidAPIFormat extends \Exception
+class InvalidAPIFormat extends \Exception
+{
+    public function __construct(string $message = "", int $code = 0, \Exception $previous = null)
     {
-        public function __construct(string $message = "", int $code = 0, \Exception $previous = null)
-        {
-            parent::__construct($message, $code, $previous);
-        }
-
-        public function __toString()
-        {
-            return __CLASS__ . ": [{$this->code}]: {$this->message}" . PHP_EOL;
-        }
+        parent::__construct($message, $code, $previous);
     }
+
+    public function __toString(): string
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}" . PHP_EOL;
+    }
+}
