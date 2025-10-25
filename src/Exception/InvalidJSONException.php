7b6a8dc767551f308090a2191425287be81b0606
alex <766f6964+github@gmail.com>
2025-10-25 19:09:00 +0200

    added new exceptions for parsing json/xml

---
+++ src/Exception/InvalidJSONException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace aportela\MusicBrainzWrapper\Exception;
+
+class InvalidJSONException extends \Exception
+{
+    public function __construct(string $message = "", int $code = 0, ?\Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString(): string
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}" . PHP_EOL;
+    }
+}
