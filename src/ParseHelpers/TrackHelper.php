9c34dc3830f3b3cdbcfed47d87b99ed1355f29bc
alex <766f6964+github@gmail.com>
2025-10-27 15:47:07 +0100

    added track helper classes

---
+++ src/ParseHelpers/TrackHelper.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace aportela\MusicBrainzWrapper\ParseHelpers;
+
+class TrackHelper
+{
+    public string $mbId;
+    public int $position;
+    public int $number;
+    public \aportela\MusicBrainzWrapper\ParseHelpers\RecordingHelper $recording;
+}
