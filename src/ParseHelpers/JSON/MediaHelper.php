0ebbc055933b56c61c8470d59d49a29209014f19
alex <766f6964+github@gmail.com>
2025-10-27 16:41:47 +0100

    warning fixed

--- src/ParseHelpers/JSON/MediaHelper.php
+++ src/ParseHelpers/JSON/MediaHelper.php
@@ -7,7 +7,8 @@ class MediaHelper extends \aportela\MusicBrainzWrapper\ParseHelpers\MediaHelper
     public function __construct(object $object)
     {
         $this->mbId = (string)$object->id;
-        $this->position = intval($object->position);
+        // on search api response, this json property is missing
+        $this->position = intval($object->position ?? 0);
 
         if (isset($object->{"track-count"}) && intval($object->{"track-count"}) > 0 && isset($object->tracks) && is_array($object->tracks)) {
             foreach ($object->tracks as $trackObject) {
