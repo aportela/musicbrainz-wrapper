92c496fd4088aa0daad31c2ab21920841143051f
alex <766f6964+github@gmail.com>
2025-10-27 12:05:25 +0100

    use simple properties access

--- src/ParseHelpers/JSON/Search/Artist.php
+++ src/ParseHelpers/JSON/Search/Artist.php
@@ -6,12 +6,12 @@ class Artist extends \aportela\MusicBrainzWrapper\ParseHelpers\ParseJSONHelper
 {
     public function parse(): mixed
     {
-        if (! (isset($this->json->{"count"}) && isset($this->json->{"artists"}))) {
+        if (! (isset($this->json->count) && isset($this->json->artists))) {
             throw new \aportela\MusicBrainzWrapper\Exception\InvalidJSONException("artists count/array not found");
         }
         $results = [];
-        if ($this->json->{"count"} > 0 && is_array($this->json->{"artists"})) {
-            foreach ($this->json->{"artists"} as $artistElement) {
+        if ($this->json->count > 0 && is_array($this->json->artists)) {
+            foreach ($this->json->artists as $artistElement) {
                 $results[] = new \aportela\MusicBrainzWrapper\ParseHelpers\JSON\ArtistHelper($artistElement);
             }
         }
