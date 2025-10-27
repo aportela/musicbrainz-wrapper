991ffb9742291d1b6420af11b548470e19eb8031
alex <766f6964+github@gmail.com>
2025-10-27 15:43:58 +0100

    added BaseHelper class (common methods)

---
+++ src/ParseHelpers/BaseHelper.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace aportela\MusicBrainzWrapper\ParseHelpers;
+
+class BaseHelper
+{
+    public function parseDate(?string $date): ?int
+    {
+        if ($date !== null) {
+            switch (mb_strlen($date)) {
+                case 10:
+                    return (intval(date_format(date_create_from_format('Y-m-d', $date), 'Y')));
+                case 4:
+                    return (intval($date));
+                default:
+                    return (null);
+            }
+        } else {
+            return (null);
+        }
+    }
+}
