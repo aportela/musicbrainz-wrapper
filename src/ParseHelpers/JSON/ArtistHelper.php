e8b4b4c1dda8b8a7c452b01ac7ea064560825084
alex <766f6964+github@gmail.com>
2025-10-27 11:51:50 +0100

    added check for type existence

--- src/ParseHelpers/JSON/ArtistHelper.php
+++ src/ParseHelpers/JSON/ArtistHelper.php
@@ -5,11 +5,11 @@ namespace aportela\MusicBrainzWrapper\ParseHelpers\JSON;
 
 class ArtistHelper extends \aportela\MusicBrainzWrapper\ParseHelpers\ArtistHelper
 {
-    public function __construct(object $element)
+    public function __construct(object $object)
     {
-        $this->mbId = (string)$element->{"id"};
-        $this->type = \aportela\MusicBrainzWrapper\ArtistType::fromString($element->{"type"}) ?: \aportela\MusicBrainzWrapper\ArtistType::NONE;
-        $this->name = (string)$element->{"name"};
-        $this->country = isset($element->{"country"}) ? (!empty($country = $element->{"country"}) ? mb_strtolower($country) : null) : null;
+        $this->mbId = (string)$object->id;
+        $this->type = isset($object->type) ? (\aportela\MusicBrainzWrapper\ArtistType::fromString($object->type) ?: \aportela\MusicBrainzWrapper\ArtistType::NONE) : \aportela\MusicBrainzWrapper\ArtistType::NONE;
+        $this->name = (string)$object->name;
+        $this->country = isset($object->country) ? (!empty($country = $object->country) ? mb_strtolower($country) : null) : null;
     }
 }
--- src/ParseHelpers/XML/ArtistHelper.php
+++ src/ParseHelpers/XML/ArtistHelper.php
@@ -8,7 +8,7 @@ class ArtistHelper extends \aportela\MusicBrainzWrapper\ParseHelpers\ArtistHelpe
     public function __construct(\SimpleXMLElement $element)
     {
         $this->mbId = (string) $element->attributes()->id;
-        $this->type = \aportela\MusicBrainzWrapper\ArtistType::fromString($element->attributes()->type) ?: \aportela\MusicBrainzWrapper\ArtistType::NONE;
+        $this->type = isset($element->attributes()->type) ? (\aportela\MusicBrainzWrapper\ArtistType::fromString($element->attributes()->type) ?: \aportela\MusicBrainzWrapper\ArtistType::NONE) : \aportela\MusicBrainzWrapper\ArtistType::NONE;
         $this->name = (string) $element->children()->name;
         $this->country = isset($element->children()->country) ?  (!empty($country = $element->children()->country) ? mb_strtolower($country) : null) : null;
     }
